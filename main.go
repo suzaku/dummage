ec4c9f2f9a00ece36aa141fe50aa62e73838dfcf
satoru <satorulogic@gmail.com>
2016-03-08 14:27:58 +0800

    Add support for parsing background RGB values from the image name

--- main.go
+++ main.go
@@ -17,41 +17,79 @@ import (
 )
 
 var imageNamePattern *regexp.Regexp
+var defaultColor = color.RGBA{0, 0xFF, 0, 0xCC}
 
-func init() {
-	imageNamePattern = regexp.MustCompile(`(\d+)x(\d+).jpg`)
+type imageConfig struct {
+	width, height int
+	background    color.Color
 }
 
-func parseDimension(name string) (int, int, error) {
+func init() {
+	imageNamePattern = regexp.MustCompile(`(?i)(\d+)x(\d+)(\-[0-9a-f]{6})?.jpg`)
+
+}
+
+func parseImageConfig(name string) (*imageConfig, error) {
 	match := imageNamePattern.FindStringSubmatch(name)
-	if len(match) == 0 {
+	if len(match) != 4 {
 		msg := fmt.Sprintf("Fail to parse name: %v", name)
-		return 0, 0, errors.New(msg)
+		return nil, errors.New(msg)
 	}
 
 	var width, height int
-	widthStr, heightStr := match[1], match[2]
+	widthStr, heightStr, colorStr := match[1], match[2], match[3]
 	width, err := strconv.Atoi(widthStr)
-	if err != nil {
-		return width, height, err
+	if err == nil {
+		height, err = strconv.Atoi(heightStr)
 	}
-	height, err = strconv.Atoi(heightStr)
-	return width, height, err
+
+	if err != nil {
+		return nil, err
+	}
+
+	var background color.Color
+	if len(colorStr) > 0 {
+		colorStr = colorStr[1:]
+		background = parseColor(colorStr)
+	} else {
+		background = defaultColor
+	}
+
+	return &imageConfig{width, height, background}, err
+}
+
+func parseColor(s string) color.Color {
+	var (
+		err     error
+		r, g, b uint64
+	)
+	r, err = strconv.ParseUint(s[0:2], 16, 8)
+	if err == nil {
+		g, err = strconv.ParseUint(s[2:4], 16, 8)
+	}
+
+	if err == nil {
+		b, err = strconv.ParseUint(s[4:6], 16, 8)
+	}
+
+	if err != nil {
+		log.Printf("Fail to parse color: %v, using default color\n%s", s, err)
+		return defaultColor
+	}
+	return color.RGBA{uint8(r), uint8(g), uint8(b), 0xFF}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := strings.TrimLeft(r.URL.String(), "/")
 
-	width, height, err := parseDimension(name)
+	config, err := parseImageConfig(name)
 	if err != nil {
 		log.Println(err)
 		http.NotFound(w, r)
 		return
 	}
 
-	background := color.RGBA{0, 0xFF, 0, 0xCC}
-
-	img := createImage(width, height, background)
+	img := createImage(config.width, config.height, config.background)
 	err = writeJPEG(w, img)
 	if err != nil {
 		log.Panic(err)
@@ -76,7 +114,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(url, nil))
 }
 
-func createImage(width int, height int, background color.RGBA) *image.RGBA {
+func createImage(width int, height int, background color.Color) *image.RGBA {
 	rect := image.Rect(0, 0, width, height)
 	img := image.NewRGBA(rect)
 	draw.Draw(img, img.Bounds(), &image.Uniform{background}, image.ZP, draw.Src)
