b9f0cb8025c103e60468471d67ad9ba6ef319adc
satoru <satorulogic@gmail.com>
2016-03-19 16:14:24 +0800

    Generate random tests

--- dummage_test.go
+++ dummage_test.go
@@ -1,37 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
+	"math/rand"
+	"strings"
 	"testing"
+	"time"
 )
 
-func TestParseImageSizeOnly(t *testing.T) {
-	tests := []struct {
-		name          string
-		width, height int
-	}{
-		{"200x300.jpg", 200, 300},
-		{"100x100.png", 100, 100},
-		{"500x450.jpg", 500, 450},
+type imgConfig struct {
+	name   string
+	w, h   int
+	bg     color.Color
+	format string
+}
+
+var r = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+
+func randImgConfig(r *rand.Rand, withColor bool) *imgConfig {
+	var c imgConfig
+	c.w = r.Intn(1000) + 1
+	c.h = r.Intn(1000) + 1
+	c.bg = randomColor()
+	if n := r.Float32(); n < 0.5 {
+		c.format = "PNG"
+	} else {
+		if n > 0.25 {
+			c.format = "JPEG"
+		} else {
+			c.format = "JPG"
+		}
 	}
-	for _, s := range tests {
+	if withColor {
+		r, b, g, _ := c.bg.RGBA()
+		c.name = fmt.Sprintf(
+			"%dx%d-%0x%0x%0x.%s",
+			c.w, c.h, uint8(r), uint8(g), uint8(b), strings.ToLower(c.format),
+		)
+	} else {
+		c.name = fmt.Sprintf(
+			"%dx%d.%s",
+			c.w, c.h, strings.ToLower(c.format),
+		)
+	}
+	return &c
+}
+
+func TestParseImageSizeOnly(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		s := randImgConfig(r, false)
 		width, height, _, _, _ := parseImageConfig(s.name)
-		if width != s.width || height != s.height {
+		if width != s.w || height != s.h {
 			t.Errorf("parseImageConfig(%q): (%q, %q)", s.name, width, height)
 		}
 	}
 }
 
 func TestParseImageFormat(t *testing.T) {
-	tests := []struct {
-		name   string
-		format string
-	}{
-		{"200x300.jpg", "JPG"},
-		{"100x100.png", "PNG"},
-		{"500x450.jpeg", "JPEG"},
-	}
-	for _, s := range tests {
+	for i := 0; i < 10; i++ {
+		s := randImgConfig(r, false)
 		_, _, _, format, _ := parseImageConfig(s.name)
 		if format != s.format {
 			t.Errorf("parseImageConfig(%q): %q", s.name, format)
@@ -63,6 +91,7 @@ func TestParseColor(t *testing.T) {
 		{"FF0000", 255, 0, 0},
 		{"0f1020", 15, 16, 32},
 	}
+
 	for _, s := range tests {
 		bg := parseColor(s.name).(color.RGBA)
 		if bg.R != s.r || bg.G != s.g || bg.B != s.b {
